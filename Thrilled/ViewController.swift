ede2d4f64a78500856596f73fdfdcd06658781a5
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-08-06 01:15:42 -0400

    Edits

--- Thrilled/ViewController.swift
+++ Thrilled/ViewController.swift
@@ -21,7 +21,19 @@ class ViewController: UIViewController {
     
     func sideMenu() {
         
-        
+        if revealViewController() != nil {
+            
+            menuButton.target = self
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            revealViewController().rearViewRevealWidth = 275
+            revealViewController().rightViewRevealWidth = 160
+            
+            alertButton.target = revealViewController()
+            alertButton.action = #selector(SWRevealViewController.rightRevealToggle(_:))
+            
+            
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
     }
 
 } // End Class
