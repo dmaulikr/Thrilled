1df10ac6b17c67216fbf337e80e1ee40a2406ed0
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-08-09 15:00:26 -0400

    Edits

--- Thrilled/RegisterViewController.swift
+++ Thrilled/RegisterViewController.swift
@@ -7,29 +7,51 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import SVProgressHUD
 
 class RegisterViewController: UIViewController {
+    
+    @IBOutlet var emailField: UITextField!
+    @IBOutlet var passwordField: UITextField!
+    
+    @IBAction func registerButton(_ sender: Any) {
+        
+        register()
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func register() {
+        
+        //Error
+        
+        if emailField.text == "" || passwordField.text == "" {
+            
+            SVProgressHUD.showError(withStatus: "Please Enter Your Info!")
+            
+        } else {
+            
+            //Login
+            
+            SVProgressHUD.show(withStatus: "Creating User...")
+            Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!, completion: { (user, error) in
+                if error == nil {
+                    
+                    SVProgressHUD.dismiss()
+                    
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "HomeView")
+                    self.present(vc!, animated: true, completion: nil)
+                }
+            })
+        }
     }
-    */
 
-}
+
+} // End Class
